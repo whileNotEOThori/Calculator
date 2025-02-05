5ac5be5998db24bd6a03e9bfba91258d6361e71d
whileNotEOThori <140858944+whileNotEOThori@users.noreply.github.com>
2025-02-05 21:25:02 +0200

    Added buttons components to panel and panel component to the Frame components

--- Calculator/src/App.java
+++ Calculator/src/App.java
@@ -1,8 +1,11 @@
 
-import javax.swing.JButton;
+/*import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JTextField;*/
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.*;
 
 public class App {
 
@@ -20,9 +23,73 @@ public class App {
     App() {
         window = new JFrame("Calculator");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(420, 450);
+        window.setSize(420, 550);
         window.setLayout(null);
 
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setEditable(false);
+
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Delete");
+        clrButton = new JButton("Clear");
+
+        operationButtons[0] = addButton;
+        operationButtons[1] = subButton;
+        operationButtons[2] = mulButton;
+        operationButtons[3] = divButton;
+        operationButtons[4] = decButton;
+        operationButtons[5] = equButton;
+        operationButtons[6] = delButton;
+        operationButtons[7] = clrButton;
+
+        for (int i = 0; i < 8; i++) {
+            // button.addActionListener(this);
+            operationButtons[i].setFocusable(false);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            numButtons[i] = new JButton(String.valueOf(i));
+            // numButtons[i].addActionListener(this);
+            numButtons[i].setFocusable(false);
+        }
+
+        delButton.setBounds(50, 430, 145, 50);
+        clrButton.setBounds(205, 430, 145, 50);
+        // delButton.setVisible(true);
+        // clrButton.setVisible(true);
+
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        panel.setBackground(Color.GRAY);
+
+        panel.add(numButtons[1]);
+        panel.add(numButtons[2]);
+        panel.add(numButtons[3]);
+        panel.add(addButton);
+        panel.add(numButtons[4]);
+        panel.add(numButtons[5]);
+        panel.add(numButtons[6]);
+        panel.add(subButton);
+        panel.add(numButtons[7]);
+        panel.add(numButtons[8]);
+        panel.add(numButtons[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numButtons[0]);
+        panel.add(equButton);
+        panel.add(divButton);
+
+        window.add(panel);
+        window.add(delButton);
+        window.add(clrButton);
+        window.add(textField);
         window.setVisible(true);
     }
 
